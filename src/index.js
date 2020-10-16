36d4ba5c40731edb6e3df8bcfd232314c332ebfe
TatyanaBojarkina <galaktozat@yandex.ru>
2020-10-16 14:44:11 +0400

    init: start test Signed-off-by: TatyanaBojarkina <galaktozat@yandex.ru>

--- src/index.js
+++ src/index.js
@@ -38,7 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	var arr =expr.split('');
+	var arr1 =[];
+	var str ='';
+	var str_fin ='';
+	let k =0;
+	for(let i = 0; i < arr.length + 1; i++)
+	{
+		if(k <10)
+		{
+			arr1.push(arr[i]);
+			k++;
+		}else
+		{
+			if(arr1[0] == '*')
+			{
+				str_fin = str_fin + ' ';
+				k=0;
+			arr1.splice(0, arr1.length);
+			str = '';
+			i = i-1;
+				
+			}else{
+				for(let j = 0; j < arr1.length; j=j+2){
+					if(arr1[j]==1 && arr1[j+1]==0)
+					{
+						str = str + '.';
+					}
+					if(arr1[j]==1 && arr1[j+1]==1)
+					{
+						str = str + '-';
+					}
+					
+				}
+				str_fin = str_fin + MORSE_TABLE[str];
+				
+				k=0;
+				arr1.splice(0, arr1.length);
+				str = '';
+				i = i-1;
+			}
+		}
+	}
+	return str_fin;
 }
 
 module.exports = {
